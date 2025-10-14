54258b482926f68b3f6d9371a5f60cb150efc429
junhongo-ccs <hongouj@nttdata-ccs.co.jp>
2025-10-14 03:09:41 +0000

    Fix CSP: allow blob: URLs for image preview

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
     "img-src 'self' data: blob:; " +
     "font-src 'self' data: https://fonts.gstatic.com; " +
-    "connect-src 'self' https://generativelanguage.googleapis.com https://www.gstatic.com https://fonts.gstatic.com; " +
+    "connect-src 'self' https://generativelanguage.googleapis.com https://www.gstatic.com https://fonts.gstatic.com blob:; " +
     "worker-src 'self' blob:; " +
     "object-src 'none'; " +
     "base-uri 'self';"
